c7577d73579d4242561107f7bc1f783eeb3a7bb5
Wahieb Talal <wahebtalal@gmail.com>
2024-08-22 20:30:11 +0300

    update readme

--- src/WNotification.php
+++ src/WNotification.php
@@ -7,7 +7,6 @@ use Filament\Notifications\Notification as BaseNotification;
 class WNotification extends BaseNotification
 {
     protected string $view = 'wnotifications::notification';
-    protected string $viewIdentifier = 'wnotification';
 
 
 
